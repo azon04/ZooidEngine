8366cc7ce00ec7ac50e35732cc8123569445e8cc
DESKTOP-6C5DGPK\Ahmad Fauzan <azon04@gmail.com>
2020-09-27 18:55:19 -0700

    Add Win64 platform configuration; Add x64 version of dependency binary files.

--- premake5.lua
+++ premake5.lua
@@ -21,22 +21,36 @@ newoption {
 }
 
 newoption {
+    trigger = "with-dx11",
+    description = "Using DirectX11 as rendering Graphics API."
+}
+
+newoption {
     trigger = "with-physX",
-    description = "Using OpenGL as rendering Graphics API."
+    description = "Using PhysX as main physics engine."
 }
 
 workspace "ZooidEngine"
     location ("./")
     configurations { "Debug", "Release" }
+    platforms { "Win32", "Win64" }
+
+    staticruntime "off"
+
+    filter { "platforms:Win32" }
+        system "Windows"
+        architecture "x86"
+    
+    filter { "platforms:Win64" }
+        system "Windows"
+        architecture "x86_64"
 
 project "ZooidEngine"
     location (project_dir .. "/".. _ACTION .."/ZooidEngine")
     kind "StaticLib"
     language "C++"
-    
-    staticruntime "on"
 
-    targetdir "Binaries/%{cfg.buildcfg}"
+    targetdir "Binaries/%{cfg.platform}/%{cfg.buildcfg}"
 
     includedirs { 
         "Dependencies/freetype-2.9.1/include",
@@ -54,10 +68,11 @@ project "ZooidEngine"
     
     -- exclude this file since it will be added on Platform specific
     removefiles { 
-        "ZooidEngine/Renderer/GL/**" 
+        "ZooidEngine/Renderer/GL/**",
+        "ZooidEngine/Renderer/DX11/**",
     }
 
-    defines { "WIN32", "_CONSOLE", "_UNICODE", "UNICODE", "_CRT_SECURE_NO_WARNINGS" }
+    defines { "_CONSOLE", "_UNICODE", "UNICODE", "_CRT_SECURE_NO_WARNINGS" }
 
     filter "configurations:Debug"
         defines { "DEBUG" }
@@ -67,17 +82,35 @@ project "ZooidEngine"
         defines { "NDEBUG" }
         optimize "On"
     
-    filter "action:vs2015"
+    filter { "platforms:Win32", "action:vs2015" }
         postbuildcommands {
             "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/win32/lib-vs2015/debug/*.dll") .. "\" %{cfg.targetdir}",
             "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/win32/lib-vs2015/debug/*.pdb") .. "\" %{cfg.targetdir}"
         }
 
-    filter "action:vs2017"
+    filter { "platforms:Win32", "action:vs2017" }
         postbuildcommands {
             "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/win32/lib-vs2017/debug/*.dll") .. "\" %{cfg.targetdir}",
             "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/win32/lib-vs2017/debug/*.pdb") .. "\" %{cfg.targetdir}"
         }
+
+    filter { "platforms:Win64", "action:vs2015" }
+        postbuildcommands {
+            "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/x64/lib-vs2015/debug/*.dll") .. "\" %{cfg.targetdir}",
+            "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/x64/lib-vs2015/debug/*.pdb") .. "\" %{cfg.targetdir}"
+        }
+
+    filter { "platforms:Win64", "action:vs2017" }
+        postbuildcommands {
+            "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/x64/lib-vs2017/debug/*.dll") .. "\" %{cfg.targetdir}",
+            "{COPY} \"" .. path.getabsolute("./Dependencies/freetype-2.9.1/x64/lib-vs2017/debug/*.pdb") .. "\" %{cfg.targetdir}"
+        }
+    
+    filter { "platforms:Win32" }
+        defines{ "WIN32" }
+    
+    filter { "platforms:Win64" }
+        defines{ "WIN64" }
     
     -- basic configuration block for OpenGL
     configuration "with-opengl"
@@ -91,10 +124,26 @@ project "ZooidEngine"
 
         defines { "ZE_RENDER_OPENGL=1" }
 
-        postbuildcommands {
-            "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/Win32/*.DLL") .. "\" %{cfg.targetdir}",
-            "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/Win32/*.DLL") .. "\" %{cfg.targetdir}",
-        }
+        filter { "platforms:Win32" }
+            postbuildcommands {
+                "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/Win32/*.DLL") .. "\" %{cfg.targetdir}",
+                "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/Win32/*.DLL") .. "\" %{cfg.targetdir}",
+            }
+        
+        filter { "platforms:Win64" }
+            postbuildcommands {
+                "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/x64/*.DLL") .. "\" %{cfg.targetdir}",
+                "{COPY} \"" .. path.getabsolute("./Dependencies/glew-2.0.0/bin/Release/x64/*.DLL") .. "\" %{cfg.targetdir}",
+            }
+    
+    -- basic configuration block for DirectX11
+    configuration "with-dx11"
+        
+        files { "ZooidEngine/Renderer/DX11/**" }
+
+        directSDKPath = os.getenv( "DXSDK_DIR" )
+        sysincludedirs { directSDKPath.."Include" }
+        syslibdirs { directSDKPath.."Lib/x86" }
 
     -- basic configuration block for PhysX
     configuration "with-physX"
@@ -104,7 +153,7 @@ project "ZooidEngine"
             "$(Physx)/Include"
         }
     
-    filter { "options:with-physX","action:vs2015" }
+    filter { "options:with-physX", "action:vs2015", "platforms:Win32" }
         postbuildcommands {
             "{COPY} \"$(PhysX)/Bin/vc14win32/*DEBUG_x86.DLL\" %{cfg.targetdir}",
             "{COPY} \"$(PhysX)/Bin/vc14win32/nvToolsExt32_1.DLL\" %{cfg.targetdir}",
@@ -113,7 +162,7 @@ project "ZooidEngine"
             "{COPY} \"$(PhysX)/../PxShared/bin/vc14win32/*DEBUG_x86.pdb\" %{cfg.targetdir}",
         }
     
-    filter { "options:with-physX","action:vs2017" }
+    filter { "options:with-physX","action:vs2017", "platforms:Win32" }
         postbuildcommands {
             "{COPY} \"$(PhysX)/Bin/vc15win32/*DEBUG_x86.DLL\" %{cfg.targetdir}",
             "{COPY} \"$(PhysX)/Bin/vc15win32/nvToolsExt32_1.DLL\" %{cfg.targetdir}",
@@ -122,21 +171,44 @@ project "ZooidEngine"
             "{COPY} \"$(PhysX)/../PxShared/bin/vc15win32/*DEBUG_x86.pdb\" %{cfg.targetdir}",
         }
 
+    filter { "options:with-physX", "action:vs2015", "platforms:Win64" }
+        postbuildcommands {
+            "{COPY} \"$(PhysX)/Bin/vc14win64/*DEBUG_x64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/Bin/vc14win64/nvToolsExt64_1.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/Bin/vc14win64/PhysXDevice64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/../PxShared/bin/vc14win64/*DEBUG_x64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/../PxShared/bin/vc14win64/*DEBUG_x64.pdb\" %{cfg.targetdir}",
+        }
+    
+    filter { "options:with-physX","action:vs2017", "platforms:Win64" }
+        postbuildcommands {
+            "{COPY} \"$(PhysX)/Bin/vc15win64/*DEBUG_x64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/Bin/vc15win64/nvToolsExt64_1.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/Bin/vc15win64/PhysXDevice64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/../PxShared/bin/vc15win64/*DEBUG_x64.DLL\" %{cfg.targetdir}",
+            "{COPY} \"$(PhysX)/../PxShared/bin/vc15win64/*DEBUG_x64.pdb\" %{cfg.targetdir}",
+        }
+
 group "Demo"
 
 -- BEGIN: Default value for all project under this group
 function setup_game_project()
-    defines { "WIN32" }
 
-    targetdir "Binaries/%{cfg.buildcfg}"
-        
     includedirs {
         "ZooidEngine",
         "./"
     }
+    
+    targetdir "Binaries/%{cfg.platform}/%{cfg.buildcfg}"
 
-    staticruntime "on"
+    dependson { "ZooidEngine" }
 
+    filter { "platforms:Win32" }
+        defines { "WIN32" }
+
+    filter { "platforms:Win64" }
+        defines { "WIN64" }
+        
     filter "configurations:Debug"
             defines { "DEBUG" }
             symbols "On"
@@ -145,41 +217,65 @@ function setup_game_project()
         defines { "NDEBUG" }
         optimize "On"
 
-    dependson { "ZooidEngine" }
-
     configuration "*" 
         links { "ZooidEngine", "freetype" }
 
-    filter "action:vs2015"
+    filter { "action:vs2015", "platforms:Win32" }
         libdirs {
             "./Dependencies/freetype-2.9.1/win32/lib-vs2015/debug"
         }
     
-    filter "action:vs2017"
+    filter { "action:vs2017", "platforms:Win32" }
         libdirs {
             "./Dependencies/freetype-2.9.1/win32/lib-vs2017/debug"
+        }
+        
+    filter { "action:vs2015", "platforms:Win64" }
+        libdirs {
+            "./Dependencies/freetype-2.9.1/x64/lib-vs2015/debug"
+        }
+
+    filter { "action:vs2017", "platforms:Win64" }
+        libdirs {
+            "./Dependencies/freetype-2.9.1/x64/lib-vs2017/debug"
         }
 
     -- basic configuration block for OpenGL
     configuration "with-opengl"
-        libdirs {
-            "./Dependencies/glew-2.0.0/lib/Release/Win32"
-        }
-
         links {
             "glew32",
             "glfw3",
             "opengl32"
         }
     
-    filter { "options:with-opengl","action:vs2015" }
+    filter { "options:with-opengl", "platforms:Win32" }    
         libdirs {
-            "./Dependencies/glfw/Debug/lib-vc2015"
+            "./Dependencies/glew-2.0.0/lib/Release/Win32"
+        }
+    
+    filter { "options:with-opengl", "platforms:Win64" }    
+        libdirs {
+            "./Dependencies/glew-2.0.0/lib/Release/x64"
         }
 
-    filter { "options:with-opengl","action:vs2017" }
+    filter { "options:with-opengl", "action:vs2015", "platforms:Win32" }
         libdirs {
-            "./Dependencies/glfw/Debug/lib-vc2017"
+            "./Dependencies/glfw/Debug/win32/lib-vc2015"
+        }
+
+    filter { "options:with-opengl", "action:vs2017", "platforms:Win32"  }
+        libdirs {
+            "./Dependencies/glfw/Debug/win32/lib-vc2017"
+        }
+
+    filter { "options:with-opengl", "action:vs2015", "platforms:Win64" }
+        libdirs {
+            "./Dependencies/glfw/Debug/x64/lib-vc2015"
+        }
+
+    filter { "options:with-opengl", "action:vs2017", "platforms:Win64"  }
+        libdirs {
+            "./Dependencies/glfw/Debug/x64/lib-vc2017"
         }
     
     -- basic configuration block for PhysX
@@ -189,31 +285,59 @@ function setup_game_project()
             "LowLevelClothDEBUG",
             "LowLevelDEBUG",
             "LowLevelDynamicsDEBUG",
-            "LowLevelParticlesDEBUG",
+            "LowLevelParticlesDEBUG",            
+            "PhysX3ExtensionsDEBUG",
+            "PhysX3VehicleDEBUG",
+            "SceneQueryDEBUG",
+            "SimulationControllerDEBUG"
+        }
+
+    filter { "options:with-physX", "platforms:Win32" }
+        links {
             "PhysX3CharacterKinematicDEBUG_x86",
             "PhysX3CommonDEBUG_x86",
             "PhysX3CookingDEBUG_x86",
             "PhysX3DEBUG_x86",
-            "PhysX3ExtensionsDEBUG",
-            "PhysX3VehicleDEBUG",
-            "SceneQueryDEBUG",
-            "SimulationControllerDEBUG",
             "PsFastXmlDEBUG_x86",
             "PxFoundationDEBUG_x86",
             "PxPvdSDKDEBUG_x86",
             "PxTaskDEBUG_x86"
         }
 
-    filter { "options:with-physX","action:vs2015" }
+    filter { "options:with-physX", "platforms:Win64" }
+        links {
+            "PhysX3CharacterKinematicDEBUG_x64",
+            "PhysX3CommonDEBUG_x64",
+            "PhysX3CookingDEBUG_x64",
+            "PhysX3DEBUG_x64",
+            "PsFastXmlDEBUG_x64",
+            "PxFoundationDEBUG_x64",
+            "PxPvdSDKDEBUG_x64",
+            "PxTaskDEBUG_x64"
+        }
+
+    filter { "options:with-physX", "action:vs2015", "platforms:Win32" }
         libdirs {
             "$(Physx)/Lib/vc14win32",
             "$(Physx)/../PxShared/lib/vc14win32"
         }
 
-    filter { "options:with-physX","action:vs2017" }
+    filter { "options:with-physX", "action:vs2017", "platforms:Win32" }
         libdirs {
             "$(Physx)/Lib/vc15win32",
             "$(Physx)/../PxShared/lib/vc15win32"
+        }
+
+    filter { "options:with-physX", "action:vs2015", "platforms:Win64" }
+        libdirs {
+            "$(Physx)/Lib/vc14win64",
+            "$(Physx)/../PxShared/lib/vc14win64"
+        }
+
+    filter { "options:with-physX", "action:vs2017", "platforms:Win64" }
+        libdirs {
+            "$(Physx)/Lib/vc15win64",
+            "$(Physx)/../PxShared/lib/vc15win64"
         }
 
 end
@@ -302,6 +426,8 @@ project "FontAssetGen"
     kind "ConsoleApp"
     language "C++"
 
+    removeplatforms{ "Win64" }
+
     targetdir "Binaries/Tools"
 
     staticruntime "on"
@@ -342,6 +468,8 @@ project "ModelParser"
     location (project_dir .. "/".. _ACTION .."/Tools")
     kind "ConsoleApp"
     language "C++"
+    
+    removeplatforms{ "Win64" }
 
     targetdir "Binaries/Tools"
 
